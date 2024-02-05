3fda71ed823f52b574f1c9ab06ced813adcfd46c
Evan Sun <sunxuewu@moxiu.net>
2024-02-05 17:09:13 +0800

    style

--- app/components/Modal.jsx
+++ app/components/Modal.jsx
@@ -1,6 +1,6 @@
 import {IconClose, Link} from '~/components';
-import giftImg1 from '../../public/images/gift1.png'
-import giftImg2 from '../../public/images/gift2.png'
+import giftImg1 from '../../public/images/gift1.png';
+import giftImg2 from '../../public/images/gift2.png';
 
 /**
  * @param {{
@@ -49,40 +49,65 @@ export function Modal({children, cancelLink}) {
 
 export function DiscountModal({index, onClickBuyBtn}) {
   const clickBuyBtn = (e) => {
-    onClickBuyBtn(index)
-  }
+    onClickBuyBtn(index);
+  };
   if (index == 0) {
     return (
-      <div className='discount-box flex flex-col justify-center items-center'>
-        <div className='mb-2 w-16 h-16'>
-          <img src={giftImg1} alt="" className='w-16'/>
+      <div className="discount-box flex flex-col justify-center items-center">
+        <div className="text-2xl font-medium mb-1">Only Today!</div>
+        <div className="mb-6">
+          Get <b>5% off</b> your first order.
         </div>
-        <div className='text-2xl font-medium mb-2'>Sale!</div>
-        <div className='text-3xl font-bold mb-4'>5% DISCOUNT</div>
-        <div onClick={clickBuyBtn} className='text-2xl font-medium mb-4 w-full flex justify-center items-center h-10 rounded bg-red-500 text-white'>BUY NOW</div>
+        <div className="mb-6 w-16 h-16">
+          <img src={giftImg1} alt="" className="w-16" />
+        </div>
+        <div className="text-3xl font-bold mb-8 underline underline-offset-8">
+          5% OFF
+        </div>
+        <div
+          onClick={clickBuyBtn}
+          className="text-2xl font-medium mb-5 w-full flex justify-center items-center h-10 rounded bg-rose-500 text-white"
+        >
+          Okay, Shop Now!
+        </div>
       </div>
-    )
+    );
   } else if (index == 1) {
     return (
-      <div className='discount-box flex flex-col justify-center items-center'>
-        <div className='mb-2 w-16 h-16'>
-          <img src={giftImg2} alt="" className='w-16'/>
+      <div className="discount-box flex flex-col justify-center items-center">
+        <div className="text-2xl font-bold mb-1">Congratulations!</div>
+        <div className="font-bold mb-6">You've just won a reward!</div>
+        <div className="mb-6 w-16 h-16">
+          <img src={giftImg2} alt="" className="w-16" />
         </div>
-        <div className='text-3xl font-bold mb-4 text-rose-700'>-15% FOR ALL</div>
-        <div onClick={clickBuyBtn} className='text-2xl font-medium mb-4 w-full flex justify-center items-center h-10 rounded bg-rose-600 text-white'>BUY NOW</div>
+        <div className="text-3xl font-bold mb-8 text-rose-700">15% OFF</div>
+        <div
+          onClick={clickBuyBtn}
+          className="text-2xl font-medium mb-5 w-full flex justify-center items-center h-10 rounded bg-rose-500 text-white"
+        >
+          Claim Prize
+        </div>
       </div>
-    )
+    );
   } else {
     return (
-      <div className='discount-box flex flex-col justify-center items-center'>
-        <div className='w-full h-12 text-3xl font-bold flex justify-center items-center'>
-          New collection
+      <div className="discount-box flex flex-col justify-center items-center">
+        <div className="w-full h-12 text-3xl font-bold flex justify-center items-center">
+          Wait!
         </div>
-        <div className='text-5xl font-bold mb-4 w-full flex items-center justify-center'>20% OFF</div>
-        <div className='text-base font-medium w-full flex items-center justify-center'>Get a discount on all</div>
-        <div className='text-base font-medium mb-4 w-full flex items-center justify-center'>items in your next order</div>
-        <div onClick={clickBuyBtn} className='text-2xl font-medium mb-4 w-full flex justify-center items-center h-10 rounded bg-rose-600 text-white'>BUY NOW</div>
+        <div className="w-full h-8 mb-5 text-xl font-bold flex justify-center items-center">
+          You're about to lose
+        </div>
+        <div className="text-5xl font-bold mb-8 w-full flex items-center justify-center">
+          20% OFF
+        </div>
+        <div
+          onClick={clickBuyBtn}
+          className="text-2xl font-medium mb-5 w-full flex justify-center items-center h-10 rounded bg-rose-500 text-white"
+        >
+          Claim Now!
+        </div>
       </div>
-    )
+    );
   }
 }
